d2ed6b1e434e1035f9095288619347ad2f15d2b4
Rodrigo-Montes-9 <rodrigomontes91@aragon.unam.mx>
2024-10-02 16:28:53 -0600

    Pueba12

--- sunae.py
+++ sunae.py
@@ -216,10 +216,11 @@ if __name__ == "__main__":
      
     for a in azimuths:
         print (a)
+        time.sleep(2)
     try:
         while True:
             # Mover el servomotor al ángulo mínimo
-            mover_servomotor(a[:3])
+            mover_servomotor(a)
             # Mover el servomotor al ángulo máximo
             #mover_servomotor(a[5])
     except KeyboardInterrupt:
