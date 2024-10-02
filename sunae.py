f3d09a758af929afce8b9464162df3cd31fbeccd
Rodrigo-Montes-9 <rodrigomontes91@aragon.unam.mx>
2024-10-02 16:38:46 -0600

    Pueba16

--- sunae.py
+++ sunae.py
@@ -218,8 +218,8 @@ if __name__ == "__main__":
         print (a)
         #time.sleep(2)
     
-     try:
-        while True:
+     try: 
+        while True: 
             # Mover el servomotor al ángulo mínimo
             mover_servomotor(a)
             # Mover el servomotor al ángulo máximo
